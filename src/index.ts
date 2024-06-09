181f9fc7351a0a509b16f2b688bb31c1c11f19d8
dan-schel <me@danschellekens.com>
2024-06-09 18:09:13 +1000

    fetch methods + lint github action

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./arrays";
 export * from "./clamp";
 export * from "./encoding";
+export * from "./fetch/index";
 export * from "./scope";
 export * from "./integers";
 export * from "./strings";
