8d473636d709696b95fb6af886d455f2f657c3c1
dan-schel <me@danschellekens.com>
2025-06-14 15:39:33 +1000

    use uuid package again

--- src/uuid.ts
+++ src/uuid.ts
@@ -1,6 +1,10 @@
+import { v4 } from "uuid";
+
 /**
  * Returns a random UUID string.
  */
 export function uuid(): string {
-  return crypto.randomUUID();
+  // Using UUID package over crypto.randomUUID because the crypto API is
+  // unavailable when using HTTP instead of HTTPS (impacts testing over LAN).
+  return v4();
 }
