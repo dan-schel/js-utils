b8e6eb8f59b7a665d7ab603300ed7fe6772b1ab9
dan-schel <me@danschellekens.com>
2025-07-26 10:59:08 +1000

    add seeded-random

--- tests/zod.test.ts
+++ tests/zod.test.ts
@@ -1,5 +1,5 @@
 import { expect, describe, it } from "vitest";
-import { buildZodTransform } from "../src/zod";
+import { buildZod3Transform } from "../src/zod";
 
 const zodNever = Object.freeze({ status: "aborted" }) as never;
 
@@ -19,7 +19,7 @@ describe("buildZodTransform", () => {
     }
 
     it("returns the transformed value and doesn't add any issues", () => {
-      const transform = buildZodTransform(fn);
+      const transform = buildZod3Transform(fn);
       const ctx = createMockZodContext();
       const result = transform("42", ctx);
 
@@ -35,7 +35,7 @@ describe("buildZodTransform", () => {
       }
 
       it("returns zodNever, and adds an issue with the error message", () => {
-        const transform = buildZodTransform(fn);
+        const transform = buildZod3Transform(fn);
         const ctx = createMockZodContext();
         const result = transform("invalid", ctx);
 
@@ -49,7 +49,7 @@ describe("buildZodTransform", () => {
 
       describe("when supplied with an override error message", () => {
         it("still uses the original error message", () => {
-          const transform = buildZodTransform(fn, "Custom override message");
+          const transform = buildZod3Transform(fn, "Custom override message");
           const ctx = createMockZodContext();
           transform("invalid", ctx);
 
@@ -68,7 +68,7 @@ describe("buildZodTransform", () => {
       }
 
       it("returns zodNever, and adds an issue with a default error message", () => {
-        const transform = buildZodTransform(fn);
+        const transform = buildZod3Transform(fn);
         const ctx = createMockZodContext();
         const result = transform("invalid", ctx);
 
@@ -82,7 +82,7 @@ describe("buildZodTransform", () => {
 
       describe("when supplied with an override error message", () => {
         it("returns zodNever, and adds an issue with the override error message", () => {
-          const transform = buildZodTransform(fn, "Custom override message");
+          const transform = buildZod3Transform(fn, "Custom override message");
           const ctx = createMockZodContext();
           const result = transform("invalid", ctx);
 
